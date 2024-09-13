26d7b88bc6c8456213980e3881c4d4dfc3b16be3
Martín <magadiflo@gmail.com>
2024-09-13 11:36:24 -0500

    Angules: Actualiza servicio

--- todo-list-frontend/src/app/services/item.service.ts
+++ todo-list-frontend/src/app/services/item.service.ts
@@ -1,10 +1,10 @@
 import { inject, Injectable, NgZone } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { environment } from '../../environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Item } from '../model/item';
 import { ItemStatus } from '../model/item-status.enum';
+import { Item } from '../model/item';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +14,6 @@ export class ItemService {
   private readonly _http = inject(HttpClient);
   private readonly _ngZone = inject(NgZone);
   private readonly _baseUrl = `${environment.apiUrl}/api/v1/items`;
-
-  private static buildOptions(version: number) {
-    const headers = new HttpHeaders().set('if-match', String(version));
-    return { headers }
-  }
 
   public findAllItems(): Observable<Item> {
     return new Observable<Item>(subscriber => {
@@ -31,7 +26,7 @@ export class ItemService {
         this._ngZone.run(() => subscriber.next(item));
       }
 
-      //* Manejo de error
+      //* Manejo de errores
       eventSource.onerror = (error) => {
         if(eventSource.readyState === 0) {
           //* La conexión ha sido cerrada por el servidor
@@ -49,19 +44,19 @@ export class ItemService {
   }
 
   public addItem(description: string): Observable<Item> {
-    return this._http.post<Item>(`${this._baseUrl}`, { description });
+    return this._http.post<Item>(this._baseUrl, { description });
   }
 
   public updateDescription(id: string, version: number, description: string): Observable<Item> {
-    return this._http.patch<Item>(`${this._baseUrl}/${id}`, { description }, ItemService.buildOptions(version));
+    return this._http.patch<Item>(`${this._baseUrl}/${id}`, { description });
   }
 
   public updateStatus(id: string, version: number, status: ItemStatus): Observable<Item> {
-    return this._http.patch<Item>(`${this._baseUrl}/${id}`, { status }, ItemService.buildOptions(version));
+    return this._http.patch<Item>(`${this._baseUrl}/${id}`, { status });
   }
 
   public deleteItem(id: string, version: number): Observable<void> {
-    return this._http.delete<void>(`${this._baseUrl}/${id}`, ItemService.buildOptions(version));
+    return this._http.delete<void>(`${this._baseUrl}/${id}`);
   }
 
 }
